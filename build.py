22c202d12515370b559408836704c73912d11956
revuk100 <renato.vukovic@uni-duesseldorf.de>
2023-12-04 15:38:28 +0100

    remove talks for now

--- build.py
+++ build.py
@@ -172,12 +172,12 @@ def get_index_html():
                         {pub}
                     </div>
                 </div>
-                <div class="row" style="margin-top: 3em;">
+                <! -- <div class="row" style="margin-top: 3em;">
                     <div class="col-sm-12" style="">
                         <h4>Talks</h4>
                         {talks}
                     </div>
-                </div>
+                </div> -->
                 <div class="row" style="margin-top: 3em; margin-bottom: 1em;">
                     {footer}
                 </div>
